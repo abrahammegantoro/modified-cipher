a411c7a66c2e901e6d0dbb7c17c4112309fe554b
Kevin Sebastian <116342133+kevinssheva@users.noreply.github.com>
2024-03-12 11:07:36 +0700

    feat: text enrypt decrypt

--- lib/src/ciphers/cipher_details_view.dart
+++ lib/src/ciphers/cipher_details_view.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:modified_cipher/src/method/rc4.dart';
 
 class CipherDetailsView extends StatefulWidget {
   final String title;
@@ -18,6 +19,7 @@ class CipherDetailsView extends StatefulWidget {
 
 class _CipherDetailsViewState extends State<CipherDetailsView> {
   late TextEditingController _inputTextController;
+  late TextEditingController _keyTextController;
   late String _resultText;
   bool _isEncryptMode = true; // Default mode is encryption
   bool _isTextMode = true; // Default input mode is text
@@ -26,43 +28,67 @@ class _CipherDetailsViewState extends State<CipherDetailsView> {
   void initState() {
     super.initState();
     _inputTextController = TextEditingController();
+    _keyTextController = TextEditingController();
     _resultText = '';
   }
 
   @override
   void dispose() {
     _inputTextController.dispose();
+    _keyTextController.dispose();
     super.dispose();
   }
 
   Future<void> _processData() async {
+    String key = _keyTextController.text;
     if (_isTextMode) {
       // Text input mode
       String inputText = _inputTextController.text;
       // Perform encryption or decryption based on selected mode
-      _resultText =
-          _isEncryptMode ? encryptText(inputText) : decryptText(inputText);
+      _resultText = _isEncryptMode
+          ? encryptText(inputText, key)
+          : decryptText(inputText, key);
     } else {
       // File input mode
       // Read file contents
       String fileContents = await _readFile();
       // Perform encryption or decryption based on selected mode
-      _resultText = _isEncryptMode
-          ? encryptText(fileContents)
-          : decryptText(fileContents);
+      // _resultText = _isEncryptMode
+      //     ? encryptText(fileContents, key)
+      //     : decryptText(fileContents, key);
     }
 
     setState(() {});
   }
 
-  String encryptText(String input) {
+  String encryptText(String input, String key) {
     // Implement your encryption logic here
-    return input;
+    if (key.isNotEmpty && input.isNotEmpty) {
+      List<int> keyBytes = key.codeUnits;
+      List<int> inputBytes = input.codeUnits;
+
+      RC4 rc4 = RC4(keyBytes);
+      List<int> resultBytes = rc4.encrypt(inputBytes);
+      String base64 = base64Encode(resultBytes);
+      return base64;
+    }
+    return "Please enter a valid key and input text";
   }
 
-  String decryptText(String input) {
-    // Implement your decryption logic here
-    return input;
+  String decryptText(String input, String key) {
+    if (input.isNotEmpty && key.isNotEmpty) {
+      List<int> keyBytes = key.codeUnits;
+
+      // Decode Base64 input
+      List<int> inputBytes = base64Decode(input);
+
+      RC4 rc4 = RC4(keyBytes);
+      List<int> decryptedBytes = rc4.decrypt(inputBytes);
+
+      return String.fromCharCodes(decryptedBytes);
+    }
+
+    return '';
   }
 
   Future<String> _readFile() async {
@@ -129,23 +155,41 @@ class _CipherDetailsViewState extends State<CipherDetailsView> {
             _isTextMode
                 ? TextField(
                     controller: _inputTextController,
-                    decoration: const InputDecoration(
-                      labelText: 'Input Text',
+                    decoration: InputDecoration(
+                      labelText: _isEncryptMode
+                          ? 'Plain text'
+                          : 'Cipher text (base 64)', // Change label based on input mode
                     ),
                   )
                 : const SizedBox(), // Hide text input when file mode is selected
+            const SizedBox(height: 16),
+            TextField(
+              controller: _keyTextController,
+              decoration: const InputDecoration(
+                labelText: 'Key',
+              ),
+            ),
             const SizedBox(height: 16),
             ElevatedButton(
               onPressed: _processData,
               child: Text(_isEncryptMode ? 'Encrypt' : 'Decrypt'),
             ),
             const SizedBox(height: 16),
-            const Text('Result:'),
+            const Text('Result:',
+                style: TextStyle(fontWeight: FontWeight.bold)),
             Expanded(
               child: SingleChildScrollView(
                 child: Text(_resultText),
               ),
             ),
+            // const SizedBox(height: 16),
+            // const Text('Base 64:',
+            //     style: TextStyle(fontWeight: FontWeight.bold)),
+            // Expanded(
+            //   child: SingleChildScrollView(
+            //     child: Text(_resultBase64),
+            //   ),
+            // ),
           ],
         ),
       ),
---
+++ lib/src/method/rc4.dart
@@ -0,0 +1,43 @@
+class RC4 {
+  List<int> _key;
+  List<int> _s;
+
+  RC4(List<int> key)
+      : _key = List<int>.from(key),
+        _s = List<int>.generate(256, (index) => index) {
+    _initialize();
+  }
+
+  void _initialize() {
+    int j = 0;
+    for (int i = 0; i < 256; i++) {
+      j = (j + _s[i] + _key[i % _key.length]) % 256;
+      int temp = _s[i];
+      _s[i] = _s[j];
+      _s[j] = temp ^ _key[i % _key.length]; // XOR with the vigenere key
+    }
+  }
+
+  List<int> _process(List<int> data) {
+    List<int> result = List<int>.from(data);
+    int i = 0;
+    int j = 0;
+    for (int k = 0; k < data.length; k++) {
+      i = (i + 1) % 256;
+      j = (j + _s[i]) % 256;
+      int temp = _s[i];
+      _s[i] = _s[j];
+      _s[j] = temp;
+      result[k] ^= _s[(_s[i] + _s[j]) % 256];
+    }
+    return result;
+  }
+
+  List<int> encrypt(List<int> data) {
+    return _process(data);
+  }
+
+  List<int> decrypt(List<int> data) {
+    return _process(data);
+  }
+}
